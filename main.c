8a83be7ed1c8e8e5fcfd77c8daf75f1cd3de5818
rellingeorge <49471351+rellingeorge@users.noreply.github.com>
2019-04-10 15:37:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n;
+   printf("enter n");
+   scanf("%d", &n);
+   if(n%2==0)
+   {
+       printf("even");
+   }
+   else
+    printf("odd");
+    return 0;
+}
